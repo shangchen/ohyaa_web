b92d6c878711e8ee85b65991a38470e36ab2c85f
root <root@localhost.localdomain>
2014-03-20 20:22:09 +0800

    first commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,5 @@
+module ApplicationHelper
+		$web_name="ohyaa"
+        $read_me="    Hi,welcome to #{$web_name}.This site is a simple web for practice.Oh~,of cause it's developing, and will be better and better! If you have some words to say about this app, you can record them by 'new post'.Let me say again, thanks for your coming!"
+	
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Test1::Application.config.secret_key_base = '5d1d90824ecf7c200f367714abcf31393bb224b8396b5de45818074644495ea797370d77a51e237ba6745ab362cd3e7a7788a60e32b2f96daf003fbf61c033b4'
