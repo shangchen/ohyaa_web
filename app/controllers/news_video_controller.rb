7014f081a147bd8edb474e7ba71b2b0f63fb326d
root <root@localhost.localdomain>
2015-01-11 23:57:03 +0800

    add rescue

--- app/controllers/news_video_controller.rb
+++ app/controllers/news_video_controller.rb
@@ -5,8 +5,13 @@ class NewsVideoController < ApplicationController
 
    def get_news()
 	    url='http://v.ifeng.com/news/'
+	    begin
 		re1=Net::HTTP.get_response(URI(url))
 		re=re1.read_body
+	    rescue
+	      error_info='sorry,server not response'
+	      return error_info
+	    end
 	    re=~/<!--\/s_itv_index_ad_rectangle_01-->.*/
 	    re1= $'.to_s 
 	    re1=~ /<\/div> <div class/
