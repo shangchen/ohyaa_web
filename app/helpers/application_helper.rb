e9fbd4e0a5132452e503f0fe3da753b4fe1a7cfc
root <root@localhost.localdomain>
2015-02-07 02:10:07 +0800

    edit front

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 		$web_name="ohyaa"
-        $read_me=": Hi,welcome to #{$web_name}.This site is a simple web for practice.Oh~,of cause it's developing, and will be better and better! If you have some words to say about this app, you can record them by 'message board'.Oh~,Let me say again, thanks for your coming! >(^_^)< "
+        $read_me=": Hi,welcome to #{$web_name}.This site is a simple web for practice.Recommended browsers are Firefox and Chrome.Oh~,of cause it's developing, and will be better and better! If you have some words to say about this app, you can record them by 'message board'.Oh~,Let me say again, thanks for your coming! >(^_^)< "
 
 $news_menu='<%= link_to "video", news_video__path %>'	
 end
